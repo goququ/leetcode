f8d0cd4679442415e9183830fb79d8a8f5181c97
Ruslan.Nasyrov <goququ@ya.ru>
2022-11-13 19:25:47 +0500

    5. Longest Palindromic Substring

---
+++ src/5.longest-palindromic-substring.test.ts
@@ -0,0 +1,43 @@
+const isPal = (s: string, start: number, end: number) => {
+  let res = "";
+
+  while (!!s[start] && !!s[end] && s[start] === s[end]) {
+    res = s.slice(start, end + 1);
+
+    start--;
+    end++;
+  }
+
+  return res;
+};
+
+function longestPalindrome(s: string): string {
+  let res = "";
+
+  for (let i = 0; i < s.length; i++) {
+    const r1 = isPal(s, i, i);
+    const r2 = isPal(s, i, i + 1);
+
+    if (r1.length > res.length) {
+      res = r1;
+    }
+    if (r2.length > res.length) {
+      res = r2;
+    }
+  }
+
+  return res;
+}
+
+const TEST_CASES = {
+  babad: "bab",
+  cbbd: "bb",
+  civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth:
+    "ranynar",
+};
+
+test("5. Longest Palindromic Substring", () => {
+  Object.entries(TEST_CASES).forEach(([input, output]) =>
+    expect(longestPalindrome(input)).toBe(output)
+  );
+});
