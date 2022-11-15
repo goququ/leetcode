b044f12644db8af61d8a8169b0b0744f0fa12e02
Ruslan.Nasyrov <goququ@ya.ru>
2022-11-16 01:04:04 +0500

    26. Remove Duplicates from Sorted Array

---
+++ src/26.remove-duplicates-from-sorted-array.easy.test.ts
@@ -0,0 +1,40 @@
+/* https://leetcode.com/problems/remove-duplicates-from-sorted-array */
+
+function removeDuplicates(nums: number[]): number {
+  let duplicates = 0;
+  let index = 0;
+  let nonDuplicateIndex = 1;
+
+  while (
+    index + duplicates < nums.length &&
+    nums[nonDuplicateIndex] !== undefined
+  ) {
+    while (nums[index] === nums[nonDuplicateIndex]) {
+      nonDuplicateIndex++;
+      duplicates++;
+    }
+
+    nums[index + 1] = nums[nonDuplicateIndex];
+
+    index++;
+    nonDuplicateIndex++;
+  }
+
+  nums.length = nums.length - duplicates;
+
+  return nums.length;
+}
+
+const TEST_CASES = [
+  { input: [1, 1, 2], output: 2 },
+  { input: [0, 0, 1, 1, 1, 2, 2, 3, 3, 4], output: 5 },
+  { input: [1], output: 1 },
+];
+
+test("5. Longest Palindromic Substring", () => {
+  TEST_CASES.forEach(({ input, output }) =>
+    expect(removeDuplicates(input)).toBe(output)
+  );
+});
+
+export {};
